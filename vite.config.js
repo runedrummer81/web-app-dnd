7f2a22c3a97acdf75fa4941838594f7b4b9784ea
runedrummer81 <runedrummer81@gmail.com>
2025-10-03 10:05:56 +0200

    start-up

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/"
+    base: "/",
   };
 
   // Change base path when building for production
   if (command !== "serve") {
-    config.base = "/react-vite-spa/"; // 👈 Replace with your GitHub repository name
+    config.base = "/web-app-dnd/"; // 👈 Replace with your GitHub repository name
   }
 
   return config;
