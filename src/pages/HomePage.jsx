62117692ebfd4d0d129b99ed951d35c00eadcbbd
runedrummer81 <runedrummer81@gmail.com>
2025-10-10 14:01:58 +0200

    first-push-til-login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,9 @@ export default function HomePage() {
         Dette bliver mega fedt!
       </h1>
       <h2 className="text-3xl font-bold text-green-700">Wassup</h2>
+      <h2 className="text-5xl font-mono font-bold text-blue-400">
+        Go big or go home!
+      </h2>
     </section>
   );
 }
