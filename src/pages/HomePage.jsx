6ef66bd43f4a0f3f0c796a447c2f6719111066ae
Trier77 <jacob.aa.trier@gmail.com>
2025-10-10 13:56:32 +0200

    trier tailwind-test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ export default function HomePage() {
       <h1 className="text-3xl font-bold text-amber-300">
         Dette bliver mega fedt!
       </h1>
+      <h2 className="text-3xl font-bold text-green-700">Wassup</h2>
     </section>
   );
 }
