487e2491dd80bc5f06224263cb5ef3aebd77565f
Trier77 <jacob.aa.trier@gmail.com>
2025-10-15 11:37:23 +0200

    loadPage og newCampaign kan hente/gemme i FB

--- src/components/CampaignComp.jsx
+++ src/components/CampaignComp.jsx
@@ -1,4 +1,4 @@
-export default function CampaignComp({ nr, date, title, template, onHover, onLeave }) {
+export default function CampaignComp({ campaignNumber, title, templateId, lastOpened, onHover, onLeave }) {
   return (
     <div
       onMouseEnter={onHover}
@@ -6,10 +6,10 @@ export default function CampaignComp({ nr, date, title, template, onHover, onLea
       className="flex flex-col bg-[#2E2C27]/70 border border-[#DACA89]/40 rounded-xl px-8 py-6 text-[#DACA89] 
                  hover:bg-[#DACA89]/10 hover:border-[#DACA89] transition-all duration-300 cursor-pointer w-full"
     >
-      <h2 className="text-3xl font-bold mb-1">{title}</h2>
-      <p className="text-sm text-gray-400 mb-2">Campaign #{nr}</p>
-      <p className="text-gray-300 italic">{template}</p>
-      <p className="text-gray-400 text-sm mt-2">{date}</p>
+       <h2 className="text-3xl font-bold mb-1">{title}</h2>
+      <p className="text-sm text-gray-400 mb-2">Campaign #{campaignNumber}</p>
+      <p className="text-gray-300 italic">Template: {templateId}</p>
+      <p className="text-gray-400 text-sm mt-2">{new Date(lastOpened.seconds * 1000).toLocaleDateString()}</p>
     </div>
   );
 }
