bc88ce169e90543e1c5513603628e62ac54a598d
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-10-21 18:25:46 +0200

    Its fucking great mate

--- src/components/BGArtwork.jsx
+++ src/components/BGArtwork.jsx
@@ -1,21 +1,39 @@
+import { useEffect, useState } from "react";
+
 export default function BGArtwork({ imageUrl }) {
+  const [visibleImage, setVisibleImage] = useState(imageUrl);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    if (!imageUrl) return;
+
+    setFadeIn(false);
+
+    const timeout = setTimeout(() => {
+      setVisibleImage(imageUrl);
+      setFadeIn(true);
+    }, 300);
+
+    return () => clearTimeout(timeout);
+  }, [imageUrl]);
+
   return (
     <div className="absolute inset-0 -z-10 bg-[#1C1B18] overflow-hidden">
       <div
-        className="absolute inset-0 bg-cover duration-300 "
+        className={`absolute inset-0 bg-cover bg-center transition-opacity duration-500 ease-in-out ${
+          fadeIn ? "opacity-100" : "opacity-0"
+        }`}
         style={{
-          backgroundImage: `url(${imageUrl || ""})`,
+          backgroundImage: `url(${visibleImage || ""})`,
         }}
       ></div>
 
       <div
         className="absolute inset-0
-  [background:linear-gradient(to_left,transparent_30%,#1C1B18_80%),linear-gradient(to_right,transparent_75%,#1C1B18_100%),linear-gradient(to_bottom,transparent_40%,#1C1B18_90%),linear-gradient(to_top,transparent_75%,#1C1B18_100%)]
-  [background-size:200px_100%,150px_100%,100%_200px,100%_150px]
-
-  [background-position:left_center,right_center,center_bottom,center_top]
-  [background-repeat:no-repeat]
-"
+          [background:linear-gradient(to_left,transparent_30%,#1C1B18_80%),linear-gradient(to_right,transparent_75%,#1C1B18_100%),linear-gradient(to_bottom,transparent_40%,#1C1B18_90%),linear-gradient(to_top,transparent_75%,#1C1B18_100%)]
+          [background-size:200px_100%,150px_100%,100%_200px,100%_150px]
+          [background-position:left_center,right_center,center_bottom,center_top]
+          [background-repeat:no-repeat]"
       ></div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase"; // kun db fra firebase.js
-import { doc, getDoc } from "firebase/firestore"; // doc og getDoc fra firestore
 import { Link } from "react-router";
-import { useNavigate } from "react-router";
 import BGArtwork from "../components/BGArtwork";
-import encountersImage from "/images/encounter.jpeg"; // or wherever your image is
-import informationImage from "/images/information.jpg"; // or wherever your image is
+import loadcampaignImage from "/images/loadcampaign.jpg";
+import newcampaignImage from "/images/newcampaign.jpg";
+import encountersImage from "/images/encounter.jpeg";
+import informationImage from "/images/information.jpg";
 
 export default function HomePage() {
-  const [active, setActive] = useState(null);
-  const navigate = useNavigate();
+  const [active, setActive] = useState("Continue Campaign");
+
   const [lastCampaign, setLastCampaign] = useState(null);
 
   useEffect(() => {
@@ -49,6 +49,10 @@ export default function HomePage() {
             ? encountersImage
             : active === "Information"
             ? informationImage
+            : active === "Load Campaign"
+            ? loadcampaignImage
+            : active === "New Campaign"
+            ? newcampaignImage
             : lastCampaign?.image
         }
       />
@@ -77,27 +81,41 @@ export default function HomePage() {
                   : "/info"
               }
               onMouseEnter={() => setActive(label)}
-              onMouseLeave={() => setActive(null)}
               state={
                 label === "Continue Campaign" || label === "Load Campaign"
                   ? { campaignId: lastCampaign?.id, from: "/home" }
                   : undefined
               }
-              className="uppercase group font-[var(--font)] text-2xl cursor-pointer text-[var(--secondary)] inline-block transition-all duration-200 relative hover:border-t-2 hover:border-l-2 hover:border-b-2 hover:border-[var(--secondary)] hover:p-1 hover:text-4xl"
+              className={`uppercase font-[var(--font)] text-2xl cursor-pointer text-[var(--secondary)] inline-block transition-all duration-200 relative
+    ${
+      active === label
+        ? "border-t-2 border-l-2 border-b-2 border-[var(--secondary)] p-1 text-4xl"
+        : ""
+    }
+  `}
             >
-              <span className="block group-hover:bg-[var(--primary)] group-hover:text-[var(--dark-muted-bg)] group-hover:font-bold transition-all duration-200 hover:text-3xl py-2 px-4 relative z-10">
+              <span
+                className={`block transition-all duration-200 py-2 px-4 relative z-10
+      ${
+        active === label
+          ? "bg-[var(--primary)] text-[var(--dark-muted-bg)] font-bold text-3xl"
+          : ""
+      }
+    `}
+              >
                 {label}
               </span>
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 35.9 67.5"
-                className="absolute transform scale-105 -right-8 top-1/2 -translate-y-1/2 h-full opacity-0 transition-opacity duration-200 group-hover:opacity-100 z-0"
+                className={`absolute transform scale-105 -right-8 top-1/2 -translate-y-1/2 h-full transition-opacity duration-200 z-0
+      ${active === label ? "opacity-100" : "opacity-0"}
+    `}
               >
                 <defs>
                   <style>
-                    {`.st0 { fill: none; stroke: var(--secondary);
- stroke-miterlimit: 10; stroke-width: 2px; }`}
+                    {`.st0 { fill: none; stroke: var(--secondary); stroke-miterlimit: 10; stroke-width: 2px; }`}
                   </style>
                 </defs>
                 <polyline className="st0" points="1.4 66.8 34.5 33.8 1.4 .7" />
@@ -116,55 +134,26 @@ export default function HomePage() {
 
         {/* Right column */}
         <div className="absolute  grid-cols-[auto_1fr] justify-center items-end bottom-0 right-20 p-28 transition-all duration-500">
-          <div id="text" className={active ? "hidden" : ""}>
-            <div className="flex justify-between text-1.5xl text-[var(--primary)] text-center">
-              <div>
-                {lastCampaign?.lastOpened
-                  ? lastCampaign.lastOpened.toDate().toLocaleString()
-                  : "No last opened date"}
-              </div>
-              <div>Sessions: {lastCampaign?.sessionsCount}</div>
-            </div>
-
-            <div className="flex gap-5 text-3xl font-bold text-[var(--primary)] text-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 257.6 130.8"
-                className="fill-[var(--secondary)] w-8 h-auto -scale-x-100"
-              >
-                <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
-                <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
-              </svg>
-
-              {lastCampaign?.id
-                .replace(/^camp_/, "") // remove "camp_" at the start
-                .replace(/_/g, " ") // replace underscores with spaces
-                .toUpperCase() || ""}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 257.6 130.8"
-                className="fill-[var(--secondary)] w-8 h-auto "
-              >
-                <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
-                <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
-              </svg>
-            </div>
-          </div>
+          <div
+            id="text"
+            className={`${active ? "hidden" : ""} flex flex-col items-center `}
+          ></div>
 
           {active ? (
             <div className=" text-gray-700 text-center">
               {active === "Continue Campaign" && (
                 <div>
-                  <div className="flex justify-between text-1.5xl text-[var(--primary)] text-center">
+                  <div className="flex justify-between text-2xl  text-[var(--secondary)] text-center px-30 gap-5 uppercase">
                     <div>
                       {lastCampaign?.lastOpened
-                        ? lastCampaign.lastOpened.toDate().toLocaleString()
+                        ? lastCampaign.lastOpened.toDate().toLocaleDateString()
                         : "No last opened date"}
                     </div>
+
                     <div>Sessions: {lastCampaign?.sessionsCount}</div>
                   </div>
 
-                  <div className="flex gap-5 text-3xl font-bold text-[var(--primary)] text-center">
+                  <div className="flex gap-5 text-6xl font-black text-[var(--primary)] text-center mt-4">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 257.6 130.8"
@@ -187,17 +176,13 @@ export default function HomePage() {
                       <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
                     </svg>
                   </div>
+                  <div className="text-2xl text-[var(--primary)] mt-4">
+                    {lastCampaign?.description || "No description available."}
+                  </div>
                 </div>
               )}
               {active === "Load Campaign" && (
-                <div className="text-3xl font-bold text-[var(--primary)] text-center">
-                  Last Campaign
-                </div>
-              )}
-              {active === "New Campaign" &&
-                "🗺️ Make a new campaign from a template or your own creation."}
-              {active === "Encounters" && (
-                <div className="flex gap-5 text-3xl font-bold text-[var(--primary)] text-center">
+                <div className="flex gap-5 text-4xl font-black text-[var(--primary)] text-center mt-4 uppercase">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 257.6 130.8"
@@ -206,8 +191,49 @@ export default function HomePage() {
                     <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
                     <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
                   </svg>
-
-                  <div className="uppercase">Encounter</div>
+                  Your worlds await your return.
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 257.6 130.8"
+                    className="fill-[var(--secondary)] w-8 h-auto "
+                  >
+                    <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
+                    <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
+                  </svg>
+                </div>
+              )}
+              {active === "New Campaign" && (
+                <div className="flex gap-5 text-4xl font-black text-[var(--primary)] text-center mt-4 uppercase">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 257.6 130.8"
+                    className="fill-[var(--secondary)] w-8 h-auto -scale-x-100"
+                  >
+                    <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
+                    <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
+                  </svg>
+                  Begin a story of your own.
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 257.6 130.8"
+                    className="fill-[var(--secondary)] w-8 h-auto "
+                  >
+                    <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
+                    <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
+                  </svg>
+                </div>
+              )}
+              {active === "Encounters" && (
+                <div className="flex gap-5 text-4xl font-black text-[var(--primary)] text-center mt-4 uppercase">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 257.6 130.8"
+                    className="fill-[var(--secondary)] w-8 h-auto -scale-x-100"
+                  >
+                    <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
+                    <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
+                  </svg>
+                  Create foes, friends, and stories.
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 257.6 130.8"
@@ -219,7 +245,7 @@ export default function HomePage() {
                 </div>
               )}
               {active === "Information" && (
-                <div className="flex gap-5 text-3xl font-bold text-[var(--primary)] text-center">
+                <div className="flex gap-5 text-4xl font-black text-[var(--primary)] text-center mt-4 uppercase">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 257.6 130.8"
@@ -228,8 +254,7 @@ export default function HomePage() {
                     <path d="M171.5,114.9c-.4-13.6,18-14.3,12.2-15.2-2.6-.4-5-1-7-.5-14.6-10.9-35.6-6.8-52.3-2.6-23.9,6-45.9,17.6-69.5,24.2C23.5,129.5.3,126.4.3,126.4c19.8-1.3,39.1-5.8,55.9-13.1C100.3,94.2,123.8,37.5,88.8,0c14.5,3.5,24.5,23.4,24,37.4-.2,4,5.2,4.2,6.6,1.2,8.1-2.3,12.6,6.3,11.8,14.3-1.7,16.4-19.7,29.1-33.7,33.4-2.9,1.9-.7,6.6,2.6,5.5,41.7-13.4,84.6-51.4,130.7-27.5-5.2-.2-26.1,3.6-27.2,14,.1,2.2,4.1,2.6,5.7,2.6,18.8.4,29.3,18.8,48.3,20.4-5.4,3.2-10.7,6.5-16.7,8.8-16.5,5.8-25.2,0-38-9.7-1.2-.9-4.3-1.6-3.4,1.4,2.3,7.3,4,15.4-.6,22-8.3,13.1-28.3,6.3-27.4-8.9h0Z" />
                     <path d="M17.1,72.5h0c10.8-9.3,32.3-14.9,40.8.5l.3.8c5.8-5.4,5.8-17.5-3.1-19.3,12.1-6.8,16.8-17.2,12.9-30.7-2-7-6.2-12.6-11.7-16.7,4.4,16.4-6.9,32-19.4,41.3-9.5,7-18.9,12.4-26,22.3-5.4,7.6-8.5,15.5-10.9,23.9,4.2-8.5,10.4-16.2,17.1-22h0Z" />
                   </svg>
-
-                  <div className="uppercase">Information</div>
+                  Your guide to the journey.
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 257.6 130.8"
