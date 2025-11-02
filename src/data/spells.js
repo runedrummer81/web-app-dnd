8d89f221f9254de6c59a62157566179672c0bb16
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-11-02 12:34:12 +0100

    Spells virker med video

--- src/data/spells.js
+++ src/data/spells.js
@@ -1,9 +1,23 @@
 export const spellsData = [
   {
-    id: "fire-shield",
-    name: "Fire Shield",
-    iconURL: "/spell-icons/fireshield.png", // You'll add this icon
-    effectURL: "/spell-effects/fireshield.webm",
+    id: "flaming-sphere",
+    name: "Flaming Sphere",
+    iconURL: "/spell-icons/flaming_sphere.webp", // You'll add this icon
+    effectURL: "/spell-effects/flaming_sphere.webm",
+    color: "#F1f1f1",
+  },
+  {
+    id: "darkness",
+    name: "Darkness",
+    iconURL: "/spell-icons/darkness.webp", // You'll add this icon
+    effectURL: "/spell-effects/darkness.webm",
+    color: "#FF4500",
+  },
+  {
+    id: "web",
+    name: "Web",
+    iconURL: "/spell-icons/web.webp", // You'll add this icon
+    effectURL: "/spell-effects/web.webm",
     color: "#FF4500",
   },
   // Add more spells here later
